1c3e7b39be808ea24fa442d1f899933bb59d82b1
Ric-L <trqu217@gmail.com>
2025-10-02 20:21:37 +0530

    Removed Testimonial

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Hero } from "@/components/hero"
-import { Stats } from "@/components/stats"
-import { Services } from "@/components/services"
-import { WhyChooseUs } from "@/components/why-choose-us"
-import { Testimonials } from "@/components/testimonials"
-import { CTA } from "@/components/cta"
+import { Hero } from "@/components/hero";
+import { Stats } from "@/components/stats";
+import { Services } from "@/components/services";
+import { WhyChooseUs } from "@/components/why-choose-us";
+import { Testimonials } from "@/components/testimonials";
+import { CTA } from "@/components/cta";
 
 export default function HomePage() {
   return (
@@ -12,8 +12,8 @@ export default function HomePage() {
       <Stats />
       <Services />
       <WhyChooseUs />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <CTA />
     </>
-  )
+  );
 }
